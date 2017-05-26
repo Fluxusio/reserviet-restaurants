807f95d23d2abaab765bb9ca7ac2006d93dee0ba
Gareth Davies <gareth@Gareths-MacBook-Pro.local>
2017-05-26 16:16:28 +0700

    Added custom them

---
+++ themes/custom/butte/js/cubeportfolio/main4.js
@@ -0,0 +1,191 @@
+(function($, window, document, undefined) {
+    'use strict';
+
+    // init cubeportfolio
+    $('#js-grid-lightbox-gallery').cubeportfolio({
+        filters: '#js-filters-lightbox-gallery1, #js-filters-lightbox-gallery2',
+        loadMore: '#js-loadMore-lightbox-gallery',
+        loadMoreAction: 'click',
+        layoutMode: 'grid',
+        mediaQueries: [{
+            width: 1500,
+            cols: 5
+        }, {
+            width: 1100,
+            cols: 4
+        }, {
+            width: 800,
+            cols: 4
+        }, {
+            width: 480,
+            cols: 2
+        }, {
+            width: 320,
+            cols: 1
+        }],
+        defaultFilter: '*',
+        animationType: 'rotateSides',
+        gapHorizontal: 0,
+        gapVertical: 0,
+        gridAdjustment: 'responsive',
+        caption: 'zoom',
+        displayType: 'sequentially',
+        displayTypeSpeed: 100,
+
+        // lightbox
+        lightboxDelegate: '.cbp-lightbox',
+        lightboxGallery: true,
+        lightboxTitleSrc: 'data-title',
+        lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+
+        // singlePageInline
+        singlePageInlineDelegate: '.cbp-singlePageInline',
+        singlePageInlinePosition: 'below',
+        singlePageInlineInFocus: true,
+        singlePageInlineCallback: function(url, element) {
+            // to update singlePageInline content use the following method: this.updateSinglePageInline(yourContent)
+            var t = this;
+
+            $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: 'html',
+                    timeout: 10000
+                })
+                .done(function(result) {
+
+                    t.updateSinglePageInline(result);
+
+                })
+                .fail(function() {
+                    t.updateSinglePageInline('AJAX Error! Please refresh the page!');
+                });
+        },
+    });
+})(jQuery, window, document);
+
+
+(function($, window, document, undefined) {
+    'use strict';
+
+    // init cubeportfolio
+    $('#js-grid-lightbox-gallery-2').cubeportfolio({
+        filters: '#js-filters-lightbox-gallery1, #js-filters-lightbox-gallery2',
+        loadMore: '#js-loadMore-lightbox-gallery',
+        loadMoreAction: 'click',
+        layoutMode: 'grid',
+        mediaQueries: [{
+            width: 1500,
+            cols: 5
+        }, {
+            width: 1100,
+            cols: 4
+        }, {
+            width: 800,
+            cols: 4
+        }, {
+            width: 480,
+            cols: 2
+        }, {
+            width: 320,
+            cols: 1
+        }],
+        defaultFilter: '*',
+        animationType: 'rotateSides',
+        gapHorizontal: 20,
+        gapVertical: 20,
+        gridAdjustment: 'responsive',
+        caption: 'zoom',
+        displayType: 'sequentially',
+        displayTypeSpeed: 100,
+
+        // lightbox
+        lightboxDelegate: '.cbp-lightbox',
+        lightboxGallery: true,
+        lightboxTitleSrc: 'data-title',
+        lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+
+        // singlePageInline
+        singlePageInlineDelegate: '.cbp-singlePageInline',
+        singlePageInlinePosition: 'below',
+        singlePageInlineInFocus: true,
+        singlePageInlineCallback: function(url, element) {
+            // to update singlePageInline content use the following method: this.updateSinglePageInline(yourContent)
+            var t = this;
+
+            $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: 'html',
+                    timeout: 10000
+                })
+                .done(function(result) {
+
+                    t.updateSinglePageInline(result);
+
+                })
+                .fail(function() {
+                    t.updateSinglePageInline('AJAX Error! Please refresh the page!');
+                });
+        },
+    });
+})(jQuery, window, document);
+
+(function($, window, document, undefined) {
+    'use strict';
+
+    // init cubeportfolio
+    $('#js-grid-awesome-work').cubeportfolio({
+        filters: '#js-filters-awesome-work',
+        loadMore: '#js-loadMore-awesome-work',
+        loadMoreAction: 'click',
+        layoutMode: 'grid',
+        defaultFilter: '*',
+        animationType: 'scaleSides',
+        gapHorizontal: 30,
+        gapVertical: 30,
+        gridAdjustment: 'responsive',
+        mediaQueries: [{
+            width: 1500,
+            cols: 4
+        }, {
+            width: 1100,
+            cols: 3
+        }, {
+            width: 800,
+            cols: 2
+        }, {
+            width: 480,
+            cols: 2
+        }, {
+            width: 320,
+            cols: 1
+        }],
+        caption: 'zoom',
+        displayType: 'lazyLoading',
+        displayTypeSpeed: 400,
+
+        // singlePage popup
+        singlePageDelegate: '.cbp-singlePage',
+        singlePageDeeplinking: true,
+        singlePageStickyNavigation: true,
+        singlePageCounter: '<div class="cbp-popup-singlePage-counter">{{current}} of {{total}}</div>',
+        singlePageCallback: function(url, element) {
+            // to update singlePage content use the following method: this.updateSinglePage(yourContent)
+            var t = this;
+
+            $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: 'html',
+                    timeout: 10000
+                })
+                .done(function(result) {
+                    t.updateSinglePage(result);
+                })
+                .fail(function() {
+                    t.updateSinglePage('AJAX Error! Please refresh the page!');
+                });
+        },
+    });
+})(jQuery, window, document);
